5a6cee83912fe5fdb70cce449c26280fa3727575
Alveeeep <alvir.misalimov@gmail.com>
2022-12-27 17:21:19 +0300

    Done.

--- application/routers.py
+++ application/routers.py
@@ -7,63 +7,64 @@ from sqlalchemy.orm import Session
 router = APIRouter()
 
 get_db()
+path = "/api"
 
 
-@router.get("/api/banner", tags=["Get all"], response_model=list[schemas.MainPageBanner])
+@router.get(path + "/banner", tags=["Get all"], response_model=list[schemas.MainPageBanner])
 def get_all_banner(db: Session = Depends(get_db)):
     return crud.get_all(db, models.MainPageBanner)
 
 
-@router.get("/api/blog", tags=["Get all"], response_model=list[schemas.Blog])
+@router.get(path + "/blog", tags=["Get all"], response_model=list[schemas.Blog])
 def get_all_blogs(db: Session = Depends(get_db)):
     return crud.get_all(db, models.Blog)
 
 
-@router.get("/api/footer_phones", tags=["Get all"], response_model=list[schemas.Phones])
+@router.get(path + "/footer_phones", tags=["Get all"], response_model=list[schemas.Phones])
 def get_all_phones(db: Session = Depends(get_db)):
     return crud.get_all(db, models.Phones)
 
 
-@router.get("/api/footer_addresses", tags=["Get all"], response_model=list[schemas.Addresses])
+@router.get(path + "/footer_addresses", tags=["Get all"], response_model=list[schemas.Addresses])
 def get_all_addresses(db: Session = Depends(get_db)):
     return crud.get_all(db, models.Addresses)
 
 
-@router.get("/api/footer_objects", tags=["Get all"], response_model=list[schemas.Objects])
+@router.get(path + "/footer_objects", tags=["Get all"], response_model=list[schemas.Objects])
 def get_all_objects(db: Session = Depends(get_db)):
     return crud.get_all(db, models.Objects)
 
 
-@router.get("/api/vacancies", tags=["Get all"], response_model=list[schemas.Vacancies])
+@router.get(path + "/vacancies", tags=["Get all"], response_model=list[schemas.Vacancies])
 def get_all_vacancies(db: Session = Depends(get_db)):
     return crud.get_all(db, models.Vacancies)
 
 
-@router.get("/api/banner/{id}", tags=["Get by id"], response_model=schemas.MainPageBanner)
+@router.get(path + "/banner/{id}", tags=["Get by id"], response_model=schemas.MainPageBanner)
 def get_id_banner(id, db: Session = Depends(get_db)):
     return crud.get_by_id(db, models.MainPageBanner, id)
 
 
-@router.get("/api/blog/{id}", tags=["Get by id"], response_model=schemas.Blog)
+@router.get(path + "/blog/{id}", tags=["Get by id"], response_model=schemas.Blog)
 def get_id_blog(id, db: Session = Depends(get_db)):
     return crud.get_by_id(db, models.Blog, id)
 
 
-@router.get("/api/vacancies/{id}", tags=["Get by id"], response_model=schemas.Vacancies)
+@router.get(path + "/vacancies/{id}", tags=["Get by id"], response_model=schemas.Vacancies)
 def get_id_vacancies(id, db: Session = Depends(get_db)):
     return crud.get_by_id(db, models.Vacancies, id)
 
 
-@router.get("/api/footer_phones/{id}", tags=["Get by id"], response_model=schemas.Phones)
+@router.get(path + "/footer_phones/{id}", tags=["Get by id"], response_model=schemas.Phones)
 def get_id_phones(id, db: Session = Depends(get_db)):
     return crud.get_by_id(db, models.Phones, id)
 
 
-@router.get("/api/footer_addresses/{id}", tags=["Get by id"], response_model=schemas.Addresses)
+@router.get(path + "/footer_addresses/{id}", tags=["Get by id"], response_model=schemas.Addresses)
 def get_id_addresses(id, db: Session = Depends(get_db)):
     return crud.get_by_id(db, models.Addresses, id)
 
 
-@router.get("/api/footer_objects/{id}", tags=["Get by id"], response_model=schemas.Objects)
+@router.get(path + "/footer_objects/{id}", tags=["Get by id"], response_model=schemas.Objects)
 def get_id_objects(id, db: Session = Depends(get_db)):
     return crud.get_by_id(db, models.Objects, id)
